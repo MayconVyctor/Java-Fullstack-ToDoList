45963df5cb05bb0605a09e226cff843f4494d60d
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-04-07 20:48:25 -0300

    feat(repository): add JpaRepositories para task e category

---
+++ src/main/java/com/projetoTodolist/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.projetoTodolist.repository;
+
+import com.projetoTodolist.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
