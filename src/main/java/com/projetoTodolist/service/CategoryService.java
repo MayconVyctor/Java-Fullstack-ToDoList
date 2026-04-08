daf860f4c2a8881759550d3b445ef9933034ba98
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-04-08 15:49:50 -0300

    fix: update na conexao com a porta do banco de dados e ajuste no CategoryService

--- src/main/java/com/projetoTodolist/service/CategoryService.java
+++ src/main/java/com/projetoTodolist/service/CategoryService.java
@@ -1,5 +1,32 @@
 package com.projetoTodolist.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetoTodolist.model.Category;
+import com.projetoTodolist.repository.CategoryRepository;
+
+@Service
 public class CategoryService {
     
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAllCategories() {
+        return categoryRepository.findAll();
+    }
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Optional<Category> findById(Long id) {
+        return categoryRepository.findById(id);
+    }
+    public void deleteById(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
 }
