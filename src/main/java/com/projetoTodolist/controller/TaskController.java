58a6d137f1005539d7465ba9eed5f9cbd7d40e0c
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-04-08 16:04:13 -0300

    chore: add docker-compose para configuracao do banco de dados local

---
+++ src/main/java/com/projetoTodolist/controller/TaskController.java
@@ -0,0 +1,33 @@
+package com.projetoTodolist.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetoTodolist.model.Task;
+import com.projetoTodolist.service.TaskService;
+
+@RestController
+@RequestMapping("/api/tasks")
+
+
+public class TaskController {
+    
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAll() {
+        return taskService.findAllTasks();
+    }
+
+    @PostMapping
+    public Task create(@RequestBody Task task) {
+        return taskService.save(task);
+    }
+}
